47aa5c43fdbd41e20b88753ae543547d4f6066b4
ashif9 <ashif.sadique9@gmail.com>
2025-09-20 01:22:34 +0530

    20-09-2025

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
-use Laramin\Utility\VugiChugi;
+// use Laramin\Utility\VugiChugi;
 use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -21,7 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         using:function(){
-            Route::namespace('App\Http\Controllers')->middleware([VugiChugi::mdNm()])->group(function(){
+            // Route::namespace('App\Http\Controllers')->middleware([VugiChugi::mdNm()])->group(function(){
+            Route::namespace('App\Http\Controllers')->group(function(){
                 
                 Route::middleware(['web'])
                     ->namespace('Admin')
